2ecce095732389588e68158e88c6e5f69ce3864f
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-05-08 14:29:20 +0200

    Lambda expressions
    Java 8  Lambda expressions examples

---
+++ BigDataProject/src/java8features/User.java
@@ -0,0 +1,54 @@
+/**
+ *  Author: Inigo Alonso Ruiz
+ */
+
+package java8features;
+
+/**
+ * 
+ * @author inigo
+ *
+ *         Simple User Class
+ */
+public class User {
+
+	/* Each User has a name and an age */
+	private String name;
+	private Integer age;
+
+	/**
+	 * 
+	 * @param name
+	 *            of the user
+	 * @param age
+	 *            of the user
+	 */
+	public User(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	/* Getters and Setters */
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + ", Age:" + age;
+	}
+
+}
